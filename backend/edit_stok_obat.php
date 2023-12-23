8eb7bfe438b1173013747b1e5d09402305456a28
SYaristanto <syakuraris709@gmail.com>
2023-12-23 09:46:22 +0700

    update v1.1

---
+++ backend/edit_stok_obat.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'config/db_apotek.php';
+
+if(isset($_POST['Simpan'])) {
+    global $db_connect;
+
+    $nama = mysqli_real_escape_string($db_connect, $_POST['Nama_Obat']);
+    $jenis = mysqli_real_escape_string($db_connect, $_POST['Jenis']);
+    $stok = mysqli_real_escape_string($db_connect, $_POST['Stok']);
+    $expired = mysqli_real_escape_string($db_connect, $_POST['Expired']);
+    $satuan = mysqli_real_escape_string($db_connect, $_POST['Satuan']);
+    $harga_satuan = mysqli_real_escape_string($db_connect, $_POST['Harga_Satuan']);
+
+    // Validasi data (sesuaikan dengan kebutuhan Anda)
+
+    // Lakukan operasi SQL untuk menambahkan atau mengedit stok obat
+    $query = "SELECT * FROM tbl_stok_obat WHERE nama = '" . $nama . "' AND jenis = '" . $jenis . "'";
+    $result = mysqli_query($db_connect, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Jika obat sudah ada, lakukan operasi update
+        $query = "UPDATE tbl_stok_obat SET stok = '" . $stok . "', expired = '" . $expired . "', satuan = '" . $satuan . "', harga_satuan = '" . $harga_satuan . "' WHERE nama = '" . $nama . "' AND jenis = '" . $jenis . "'";
+    } else {
+        // Jika obat belum ada, lakukan operasi insert
+        $query = "INSERT INTO tbl_stok_obat (nama, jenis, stok, expired, satuan, harga_satuan) VALUES ('" . $nama ."', '" . $jenis ."', '" . $stok . "', '" . $expired . "', '" . $satuan ."', '" . $harga_satuan . "')";
+    }
+
+    $result = mysqli_query($db_connect, $query);
+
+    if($result) {
+        echo "Stok obat berhasil diupdate/tambahkan.";       
+        header("Location:../pages/stok_obat.php");
+        exit();
+    } else {
+        echo "Gagal mengupdate/tambahkan stok obat: " . mysqli_error($db_connect);
+        header("Location:");
+        exit();
+    }
+}
+?>
