af5a5acdd1ead866c80a424bdb78cb5a1118a847
SYaristanto <syakuraris709@gmail.com>
2023-12-23 09:53:41 +0700

    update v1.1

---
+++ pages/edit_stok_obat.php
@@ -0,0 +1,71 @@
+<?php
+
+session_start();
+if($_SESSION['role']!= 'admin'){
+	header('Location:../login.php');
+	exit();
+}
+
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__) . $ds . '..' . $ds) . $ds;
+require_once("{$base_dir}pages{$ds}core{$ds}header.php");
+?>
+
+<div class="main-panel">
+			<div class="content">
+				<div class="page-inner">
+					<div class="page-header">
+						<h4 class="page-title">Ubah Stok Obat</h4>
+						<ul class="breadcrumbs">
+							<li class="nav-home">
+								<a href="dashboard.php">
+									<i class="flaticon-home"></i>
+								</a>
+							</li>
+							<li class="separator">
+								<a href="stok_obat.php">
+								<i class="flaticon-right-arrow"></i>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="#">Ubah Stok Obat</a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+						</ul>
+					</div>
+					<form action="../backend/edit_stok_obat.php" method="POST">
+					<div class="row">
+										<div class="col-lg-12">
+											<div class="form-row"> 
+											<div class="form-group col-md-6">
+												<label for="Nama_Obat">Nama Obat</label>
+												<input type="text" name="Nama_Obat" class="form-control" id="Nama_Obat">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Jenis">Jenis</label>
+												<input type="text" name="Jenis" class="form-control" id="stok">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Stok">Stok</label>
+												<input type="text" name="Stok" class="form-control" id="email2">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Expired">Expired</label>
+												<input type="date" name="Expired" class="form-control" id="password">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Satuan">Satuan</label>
+												<input type="text" name="Satuan" class="form-control" id="email2">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Harga_Satuan">Harga Satuan</label>
+												<input type="text" name="Harga_Satuan" class="form-control" id="password">
+											</div>
+											</div>
+										</div>
+									</div>
+										<div class="card-action">
+										<button type="submit" name="Simpan" class="btn btn-primary">Simpan</button>
+									</div>
