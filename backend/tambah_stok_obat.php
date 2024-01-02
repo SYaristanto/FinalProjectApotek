e4ebfd18c06c39151659ac02d83245e41423994e
SYaristanto <syakuraris709@gmail.com>
2024-01-02 08:51:14 +0700

    update v1.2

--- backend/tambah_stok_obat.php
+++ backend/tambah_stok_obat.php
@@ -5,17 +5,16 @@ require 'config/db_apotek.php';
 if(isset($_POST['tambah'])) {
     global $db_connect;
 
-    $nama = mysqli_real_escape_string($db_connect, $_POST['Nama_Obat']);
+    $nama_obat = mysqli_real_escape_string($db_connect, $_POST['Nama_Obat']);
     $jenis = mysqli_real_escape_string($db_connect, $_POST['Jenis']);
     $stok = mysqli_real_escape_string($db_connect, $_POST['Stok']);
     $expired = mysqli_real_escape_string($db_connect, $_POST['Expired']);
     $satuan = mysqli_real_escape_string($db_connect, $_POST['Satuan']);
     $harga_satuan = mysqli_real_escape_string($db_connect, $_POST['Harga_Satuan']);
-
     // Validasi data (sesuaikan dengan kebutuhan Anda)
 
     // Lakukan operasi SQL untuk menambahkan stok obat
-    $query = "INSERT INTO tbl_stok_obat (nama, jenis, stok, expired, satuan, harga_satuan) VALUES ('" . $nama ."', '" . $jenis ."', '" . $stok . "', '" . $expired . "', '" . $satuan ."', '" . $harga_satuan . "')";
+    $query = "INSERT INTO tbl_stok_obat (nama_obat, jenis, stok, expired, satuan, harga_satuan) VALUES ('" . $nama_obat ."', '" . $jenis ."', '" . $stok . "', '" . $expired . "', '" . $satuan ."', '" . $harga_satuan . "')";
     $result = mysqli_query($db_connect, $query);
 
     if($result) {
@@ -27,4 +26,8 @@ if(isset($_POST['tambah'])) {
         header("Location:");
         exit();
     }
+}
+if(isset($_POST['batal'])) {
+    header("Location:../pages/stok_obat.php");
+    exit();
 }
