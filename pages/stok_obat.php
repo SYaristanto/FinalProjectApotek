082750d71170130b43d78a777779d9feb607dc13
SYaristanto <syakuraris709@gmail.com>
2024-01-02 08:43:11 +0700

    update v1.2

--- pages/stok_obat.php
+++ pages/stok_obat.php
@@ -25,7 +25,7 @@ require_once("{$base_dir}backend{$ds}tambah_stok_obat.php");
 							    </li>
 							        <li class="separator">
 								        <i class="flaticon-right-arrow"></i>
-                                        <a href ="Data Obat"></a>
+                                        <a href =""></a>
 							        </li>
 							    <li class="nav-item">
                                     <a href="sub-item">Stok Obat</a>
@@ -40,33 +40,28 @@ require_once("{$base_dir}backend{$ds}tambah_stok_obat.php");
 							<div class="card">
 								<div class="card-header d-flex justify-content-between align-items-center">
     								<div class="justify-content-start">
-										<a href = "tambah_stok_obat.php">
+										<a href = "form_stok_obat.php">
 											<button class="btn btn-primary btn-round ml-auto" name="tambah stok obat" data-toggle="modal" data-target="#addRowModal">
 												<i class="fa fa-plus"></i>
             									Tambah Stok Obat
         									</button>
 										</a>
-    						</div>
-    							<div class="d-flex ms-auto align-items-center"> <!-- Tambahkan ms-auto di sini -->
-        							<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-       								 <button class="btn btn-outline-success" type="submit">Search</button>
     							</div>
 							</div>
 								<div class="card-body">
 									<!-- Modal -->
 									<div class="table-responsive">
-										<table id="add-row" class="display table table-striped table-hover" >
+										<table id="add-row" class="display table table-striped table-hover">
 											<thead>
 												<tr>
 													<th>No</th>
-													<th>ID Obat</th>
 													<th>Nama Obat</th>
-                                                    <th>Jenis</th>
+													<th>Jenis</th>
                                                     <th>Stok</th>
                                                     <th>Expired</th>
 													<th>Satuan</th>
 													<th>Harga Satuan</th>
-													<th>Action</th>
+													<th style="width: 10%">Action</th>
 												</tr>
 											</thead>
 											<tbody>
@@ -75,48 +70,46 @@ require_once("{$base_dir}backend{$ds}tambah_stok_obat.php");
 													$daftar_stok_obat = mysqli_query($db_connect, "SELECT * FROM tbl_stok_obat");
 													$no = 1;
 													
-													while ($tbl_stok = mysqli_fetch_array($daftar_stok_obat)){
+													while ($tbl_stok_obat = mysqli_fetch_array($daftar_stok_obat)){
 													
 													?>
 													<tr>
 														<td>
-															<?php $no++;  ?>
+															<?php echo $no++; ?>
 														</td>
 														<td>
-															<?php echo $tbl_stok ['id_obat']; ?>
+															<?php echo $tbl_stok_obat ['nama_obat']; ?>
 														</td>
 														<td>
-															<?php echo $tbl_stok ['nama']; ?>
-														</td>
-                                                    	<td>
-															<?php echo $tbl_stok ['jenis']; ?>
-														</td>
-                                                    	<td>
-															<?php echo $tbl_stok ['stok']; ?>
-														</td>
-                                                    	<td>
-															<?php echo $tbl_stok ['expired']; ?>
+															<?php echo $tbl_stok_obat ['jenis']; ?>
 														</td>
 														<td>
-															<?php echo $tbl_stok ['satuan']; ?>
+															<?php echo $tbl_stok_obat ['stok']; ?>
 														</td>
 														<td>
-															<?php echo $tbl_stok ['harga_satuan']; ?>
+															<?php echo $tbl_stok_obat ['expired']; ?>
 														</td>
-													
 														<td>
-															<div class="card-action d-flex justify-content-between">
-																<a href="edit_stok_obat.php"> 
-																<button type="submit" name="Edit" class="btn btn-info" style="margin-right: 10px;">Edit</button>
-																</a>
-																<button type="submit" name="Hapus" class="btn btn-danger">Hapus</button>
-															</div>
+															<?php echo $tbl_stok_obat ['satuan']; ?>
 														</td>
-													</tr>
-												<?php
-												}
-												?>
+														<td>
+															<?php echo $tbl_stok_obat ['harga_satuan']; ?>
+														</td>
+														<td>
+														<div class="card-action d-flex justify-content-between">
+															<a href="edit_stok_obat.php">
+                                                                <button type="submit" name="Edit" class="btn btn-primary" style="margin-right: 10px;">Edit</button>
+                                                            </a>
+															<a href="../backend/hapus_stok_obat.php?id_obat=<?php echo $tbl_stok_obat['id_obat']; ?>">
+															<button type="submit" name="Hapus" class="btn btn-danger">Hapus</button>
+														</div>
+													</td>
+												</tr>
+											<?php
+											}
+											?>
 											</tbody>
+										</div>
 										</table>
 									</div>
 								</div>
@@ -127,4 +120,4 @@ require_once("{$base_dir}backend{$ds}tambah_stok_obat.php");
 </body>
 
 <!-- <?php require_once("{$base_dir}pages{$ds}core{$ds}footer.php");
-?> -->
+?>  -->
