9dfeb8ed878f34974d13809020c450af85c44174
SYaristanto <syakuraris709@gmail.com>
2024-01-08 09:28:17 +0700

    update v1.1

--- backend/login.php
+++ backend/login.php
@@ -1,77 +1,97 @@
 <?php
-session_start();
-require'./config/db_apotek.php';
+// session_start();
+// require'./config/db_apotek.php';
 
-if (isset($_POST['login'])) {
+// if (isset($_POST['login'])) {
 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $user = mysqli_query($db_connect, "SELECT * FROM tbl_user WHERE email = '$email'");
-    if (mysqli_num_rows($user) > 0) {
-        $data = mysqli_fetch_assoc($user);
-
-        if (password_verify($password, $data['password'])) {
-
-            //otorisasi
-            $_SESSION['nama'] = $data['nama'];
-            $_SESSION['role'] = $data['role'];
-
-            if ($_SESSION['role'] == 'admin') {
-                header('Location:../pages/dashboard.php');
-                exit;
-            
-            } else if ($_SESSION['role'] == 'super_admin') {
-                header('Location:../pages/dashboard_owner.php'); // Sesuaikan dengan nama file dashboard super admin
-                exit;
-            
-            } else {
-                header('Location:../login.php');
-            }
-
-        } else {
-            echo "password salah";
-            die;
-        }
-
-    } else {
-        echo "username atau password salah";
-    die;
-}
-}
-
-
-
-// if(isset($_POST['login'])) {
 //     $email = $_POST['email'];
 //     $password = $_POST['password'];
 
-//     $user = mysqli_query($db_connect, "SELECT * FROM tbl_user WHERE email ='$email'");
-//     if(mysqli_num_rows($user) > 0 ){
+//     $user = mysqli_query($db_connect, "SELECT * FROM tbl_user WHERE email = '$email'");
+//     if (mysqli_num_rows($user) > 0) {
 //         $data = mysqli_fetch_assoc($user);
-        
-//         if(password_verify($password, $data['password'])) {
-            
-//             // Otorisasi
-//             $_SESSION['email'] = $data['email'];
+
+//         if (password_verify($password, $data['password'])) {
+
+//             //otorisasi
+//             $_SESSION['nama'] = $data['nama'];
 //             $_SESSION['role'] = $data['role'];
-//             if($_SESSION['role'] == 'admin') {
-                
+
+//             if ($_SESSION['role'] == 'admin') {
 //                 header('Location:../pages/dashboard.php');
 //                 exit;
+            
+//             } else if ($_SESSION['role'] == 'super_admin') {
+//                 header('Location:../pages/dashboard_owner.php'); // Sesuaikan dengan nama file dashboard super admin
+//                 exit;
+            
+//             } else {
+//                 header('Location:../pages/');
 //             }
-//             else{
-//                 header('Location:../../login.php');  
-//             }
-//         }
-//         else {
-//             echo"password salah";
+
+//         } else {
+//             echo "password salah";
 //             die;
 //         }
-//     }
-//     else {
-//         echo"username atau password salah";
-//         die;
-//     }
+
+//     } else {
+//         echo "username atau password salah";
+//     die;
 // }
+// }
+?>
+
+<?php
+session_start();
+require './config/db_apotek.php';
+
+class Auth {
+    private $db_connect;
+
+    public function __construct($db_connect) {
+        $this->db_connect = $db_connect;
+    }
+
+    public function login($email, $password) {
+        $user = mysqli_query($this->db_connect, "SELECT * FROM tbl_user WHERE email = '$email'");
+        
+        if (mysqli_num_rows($user) > 0) {
+            $data = mysqli_fetch_assoc($user);
+
+            if (password_verify($password, $data['password'])) {
+                $this->authorizeUser($data);
+            } else {
+                echo "Password salah";
+                die;
+            }
+        } else {
+            echo "Username atau password salah";
+            die;
+        }
+    }
+
+    private function authorizeUser($data) {
+        $_SESSION['nama'] = $data['nama'];
+        $_SESSION['role'] = $data['role'];
+
+        if ($_SESSION['role'] == 'admin') {
+            header('Location:../pages/dashboard.php');
+            exit;
+        } else if ($_SESSION['role'] == 'super_admin') {
+            header('Location:../pages/dashboard_owner.php'); // Sesuaikan dengan nama file dashboard super admin
+            exit;
+        } else {
+            header('Location:../pages/');
+            exit;
+        }
+    }
+}
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $auth = new Auth($db_connect);
+    $auth->login($email, $password);
+}
 ?>
