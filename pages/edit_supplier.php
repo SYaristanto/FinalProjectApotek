58e347cdfdb2eb626075c4e10ae0b61551215a5f
SYaristanto <syakuraris709@gmail.com>
2024-01-08 10:01:12 +0700

    first commit

---
+++ pages/edit_supplier.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+if($_SESSION['role']!= 'admin'){
+	header('Location:../login.php');
+	exit();
+}
+
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__) . $ds . '..' . $ds) . $ds;
+require_once("{$base_dir}pages{$ds}core{$ds}header.php");
+?>
+
+<div class="main-panel">
+			<div class="content">
+				<div class="page-inner">
+					<div class="page-header">
+						<h4 class="page-title">Ubah Supplier</h4>
+						<ul class="breadcrumbs">
+							<li class="nav-home">
+								<a href="dashboard.php">
+									<i class="flaticon-home"></i>
+								</a>
+							</li>
+							<li class="separator">
+								<a href="supplier.php">
+								<i class="flaticon-right-arrow"></i>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="#">Supplier</a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+						</ul>
+					</div>
+					<form action="../backend/edit_supplier.php" method="POST">
+									<div class="row">
+										<div class="col-lg-12">
+											<div class="form-row"> 
+											<div class="form-group col-md-6">
+												<label for="Nama_Supplier">Nama Supplier</label>
+												<input type="text" name="nama_supplier" class="form-control" id="nama_supplier">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Email">Email</label>
+												<input type="text" name="email" class="form-control" id="email">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="Alamat">Alamat</label>
+												<input type="text" name="alamat" class="form-control" id="alamat">
+											</div>
+											<div class="form-group col-md-6">
+												<label for="No_Telepon">No Telepon</label>
+												<input type="text" name="no_telepon" class="form-control" id="no_telepon">
+											</div>
+										</div>
+									</div>
+										<div class="card-action">
+										<button type="submit" name="simpan" class="btn btn-primary mx-2">Simpan</button>
+										<button type="submit" name="batal" class="btn btn-danger">Batal</button>
+									</div>
+					</form>
