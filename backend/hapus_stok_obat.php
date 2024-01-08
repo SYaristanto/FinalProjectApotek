cca72cc9dd5da29e03e56e01b2626fd45c2f0699
SYaristanto <syakuraris709@gmail.com>
2024-01-08 09:25:34 +0700

    update v1.1

--- backend/hapus_stok_obat.php
+++ backend/hapus_stok_obat.php
@@ -1,25 +1,23 @@
 <?php
 
 require 'config/db_apotek.php';
-// $hapus = mysqli_query($db_connect, "SELECT * FROM tbl_stok_obat");
 
 if(isset($_GET['id_obat'])) {
     global $db_connect;
 
-    // $id_obat = mysqli_real_escape_string($db_connect, $_POST['id_obat']);
-    // Lakukan operasi SQL untuk menghapus stok obat
-    mysqli_query($db_connect, "DELETE FROM tbl_stok_obat WHERE id_obat = '$_GET[id_obat]'");
-    header("Location:../pages/stok_obat.php");
-    exit();
-    // mysqli_query($db_connect,"DELETE FROM tbl_stok_obat WHERE id_obat = '$_GET[id_obat]'");
-    // header("Location:../pages/stok_obat.php");
-    // die():
+    // Hapus terlebih dahulu data di tabel anak (tbl_penjualan)
+    mysqli_query($db_connect, "DELETE FROM tbl_penjualan WHERE id_obat = '$_GET[id_obat]'");
 
-    // if($result) {
-       
-    // } else {
-    //     echo "Gagal menghapus stok obat: " . mysqli_error($db_connect);
-    //     exit();
-    // }   
+    // Hapus data di tabel induk (tbl_stok_obat)
+    $result = mysqli_query($db_connect, "DELETE FROM tbl_stok_obat WHERE id_obat = '$_GET[id_obat]'");
+
+    if($result) {
+        header("Location:../pages/stok_obat.php");
+        exit();
+    } else {
+        echo "Gagal menghapus stok obat: " . mysqli_error($db_connect);
+        header("Location:../pages/stok_obat.php");
+        exit();
+    }
 }
 ?>
